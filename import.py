606ee49ce0b03a72fa040ee1fdc728f4d9224fb3
Samuel Tremblay <samuel.tremblay@hec.ca>
2019-11-04 20:25:55 -0500

    returns train set and train_labels in the same object

--- import.py
+++ import.py
@@ -7,12 +7,15 @@ import csv
 def load_data():
     with open(join('data', 'train_labels.csv'), 'r') as f:
         reader = csv.reader(f)
-        train_labels = list(reader)
+        train_labels = dict(reader)
     train = np.load(join('data', 'train_images.npy'), encoding='latin1')
     test = np.load(join('data', 'test_images.npy'), encoding='latin1')
+    
+    for x in train:
+        x[0] = train_labels[str(x[0])]
 
-    return train_labels, train, test
+    return train, test
 
 if __name__ == '__main__':
-    x, y, z = load_data()
+    train, test = load_data()
 breakpoint()
